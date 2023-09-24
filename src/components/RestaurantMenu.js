1f0a36feec42053e57b140049ac242fcd9ea02bf
Arijeet Konar <43075962+arijeetkonar1997@users.noreply.github.com>
2023-09-24 17:14:06 +0530

    Episode 11 Controlled/Uncontrolled Components  Lifting the State Up

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,4 +1,5 @@
 
+import {useState} from 'react';
 import ShimmerMenu from './ShimmerMenu';
 import { useParams } from 'react-router-dom';
 import useRestaurantMenu from '../utils/useRestaurantMenu';
@@ -7,6 +8,7 @@ const RestaurantMenu = () => {
 
     const {resId} = useParams();
     const resInfo = useRestaurantMenu(resId)
+    const [showIndex,setShowIndex]= useState(null)
 
     if(resInfo===null)
     {
@@ -23,7 +25,7 @@ const RestaurantMenu = () => {
             <h2 className='font-bold text-lg'>Menu</h2>
             <ul>
             {/* {itemCards.map((res)=><li>{res?.card?.info?.name+" "+" Rs."+ (res?.card?.info?.price)/100}</li>)} */}
-            {categories.map((category)=> <RestaurantCategory data={category?.card?.card}/>)}
+            {categories.map((category,index)=> <RestaurantCategory setShowIndex={() => setShowIndex(index)} showItem={index===showIndex?true:false} data={category?.card?.card}/>)}
             </ul>
         </div>
     )
